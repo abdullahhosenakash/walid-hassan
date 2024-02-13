6773faa5a62eacb264a51d60adda332c1ca6cd2d
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-14 00:21:22 +0600

    theme bug fixed

--- app/_lib/updateTheme.js
+++ app/_lib/updateTheme.js
@@ -5,10 +5,5 @@ import { cookies } from 'next/headers';
 
 export async function updateTheme(theme) {
   const cookieStore = cookies();
-  cookieStore.set(USER_THEME, theme, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    path: '/'
-  });
+  cookieStore.set(USER_THEME, theme);
 }
