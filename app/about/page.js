a0c6ee9a691e41a02ff4e761cca3b6587f7043d9
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-15 00:22:34 +0600

    experience page dynamic data added

--- app/about/page.js
+++ app/about/page.js
@@ -1,17 +1,11 @@
+import { getMiscellaneousData } from '@/app/_lib/getMiscellaneousData';
 import Link from 'next/link';
 
-const About = () => {
-  const aboutText = {
-    firstPara:
-      'I am an enthusiastic Programmer. I love to solve problems and learn new things. My interests are in Web Development, Algorithm Design and Analysis, and Competitive Programming. I am also interested in Machine Learning and Artificial Intelligence. I love programming and want to do something meaningful in this field.',
-    secondPara:
-      'I am active with Competitive Programming for more than 3 years, which prepared me as a good problem solver and efficient programmer. I have been learning different Web Development technologies for the last 3 years and gathered quite a lot of experience in this field.',
-    thirdPara:
-      'I am looking for a full-time or part-time job in Web Development. I am also open to internship opportunities. Thanks for reading!'
-  };
-  const { firstPara, secondPara, thirdPara } = aboutText || {};
+const About = async () => {
+  const { aboutMe } = await getMiscellaneousData();
+  const { firstPara, secondPara, thirdPara } = aboutMe || {};
   return (
-    <section className='dark:bg-slate-900 lg:py-8 py-4 dark:text-white lg:px-0 px-3 lg:dark:h-[100vh]'>
+    <section className='dark:bg-slate-900 lg:py-8 py-4 dark:text-white lg:px-0 px-3 lg:dark:min-h-screen'>
       <h2 className='text-center text-3xl dark:text-white'>About Me</h2>
       <div className='flex flex-col gap-4 text-lg mt-6 lg:w-[65%] mx-auto text-justify'>
         <p>Hi,</p>
--- app/experience/page.js
+++ app/experience/page.js
@@ -1,86 +1,20 @@
+import { getMiscellaneousData } from '@/app/_lib/getMiscellaneousData';
 import Link from 'next/link';
 
-const Experience = () => {
+const Experience = async () => {
+  const { experience } = await getMiscellaneousData();
   return (
     <section className='dark:bg-slate-900 lg:py-8 py-4 dark:text-white lg:px-0 px-3 lg:dark:h-screen'>
       <h2 className='text-center text-3xl'>My Experience</h2>
       <div className='flex flex-col gap-6 text-lg mt-6 lg:w-[65%] mx-auto text-justify'>
-        <div>
-          <p className='font-bold text-xl hover:cursor-pointer hover:dark:text-blue-400 text-blue-700'>
-            Problem Solving
-          </p>
-          <p className='mt-1'>
-            I am an avid learner of Algorithms and Data Structures, Mathematics
-            and Problem solving strategies. I have solved 1500+ problems on
-            various platforms.{' '}
-            <Link href='' className='text-blue-700 hover:underline'>
-              View
-            </Link>{' '}
-            my problem solving stats. Also, see my{' '}
-            <Link href='' className='text-blue-700 hover:underline'>
-              Algorithm Learning
-            </Link>{' '}
-            in GitHub.
-          </p>
-        </div>
-        <div>
-          <p className='font-bold text-xl'>Project Works</p>
-          <p className='mt-1'>
-            I have worked on some interesting projects. I have experience of
-            working on both individual and team projects. Check out my{' '}
-            <Link href='' className='text-blue-700 hover:underline'>
-              projects
-            </Link>
-          </p>
-        </div>
-        <div>
-          <p className='font-bold text-xl'>Teaching</p>
-          <p className='mt-1'>
-            I have experience in teaching Computer Science students of
-            Universities in Bangladesh and abroad. I have tutored the following
-            topics both offline and online:{' '}
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              C Programming
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              C++
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Python
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Java
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Data Structures
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Algorithms
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Problem Solving
-            </span>
-            <span className='px-2 inline-block text-base mr-2 rounded-full bg-blue-700 text-white'>
-              Discrete Math
-            </span>
-          </p>
-        </div>
-        <div>
-          <p className='font-bold text-xl'>Job Experience</p>
-          <p className='mt-1'>
-            I am an avid learner of Algorithms and Data Structures, Mathematics
-            and Problem solving strategies. I have solved 1500+ problems on
-            various platforms.{' '}
-            <Link href='' className='text-blue-700 hover:underline'>
-              View
-            </Link>{' '}
-            my problem solving stats. Also, see my{' '}
-            <Link href='' className='text-blue-700 hover:underline'>
-              Algorithm Learning
-            </Link>{' '}
-            in GitHub.
-          </p>
-        </div>
+        {experience?.map((e) => (
+          <div key={e.experienceName}>
+            <p className='font-bold text-xl hover:cursor-pointer hover:dark:text-blue-400 hover:text-blue-700'>
+              <Link href={e.experienceLink}>{e.experienceName}</Link>
+            </p>
+            <p className='mt-1'>{e.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
