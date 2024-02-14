bb1863c2267f4ba174efcf9f46e8856b35ebe33d
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-15 00:39:25 +0600

    contact page dynamic data added

--- app/_lib/getMiscellaneousData.js
+++ app/_lib/getMiscellaneousData.js
@@ -3,5 +3,5 @@ import { DB } from '@/app/_utils/mongoDB';
 export async function getMiscellaneousData() {
   const { miscellaneousCollection } = await DB();
   const [user] = (await miscellaneousCollection.find()?.toArray()) || [];
-  return user;
+  return user || {};
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,39 @@
+import { getMiscellaneousData } from '@/app/_lib/getMiscellaneousData';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
-const Contact = () => {
+const ContactCard = ({ children, contactInfo }) => {
+  return (
+    <div className='text-center shadow-xl dark:shadow-none bg-slate-100 dark:bg-slate-800 py-6'>
+      <FontAwesomeIcon
+        icon={
+          children === 'Email'
+            ? faEnvelope
+            : children === 'Phone'
+            ? faPhone
+            : children === 'whatsApp'
+            ? faWhatsapp
+            : faLocationDot
+        }
+        className='text-3xl inline-block w-8'
+      />
+      <p className='text-2xl'>{children}</p>
+      <Link
+        href='mailto:walidhassan@tanim.com'
+        className='dark:text-blue-400 text-blue-700 text-xl hover:underline'
+      >
+        walidhassan@tanim.com
+      </Link>
+    </div>
+  );
+};
+
+const Contact = async () => {
+  const { contact } = await getMiscellaneousData();
+
   return (
     <section className='dark:bg-slate-900 lg:py-8 py-4 dark:text-white lg:px-0 px-3 lg:dark:h-screen'>
       <h2 className='text-center text-3xl dark:text-white'>My Contacts</h2>
@@ -12,58 +41,10 @@ const Contact = () => {
         Feel free to contact me if you think I can help somehow
       </p>
       <div className='grid lg:grid-cols-2 grid-cols-1 lg:gap-8 gap-3 mx-auto lg:w-3/4 mt-6'>
-        <div className='text-center shadow-xl dark:shadow-none bg-slate-100 dark:bg-slate-800 py-6'>
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className='text-3xl inline-block w-8'
-          />
-          <p className='text-2xl'>Email</p>
-          <Link
-            href='mailto:walidhassan@tanim.com'
-            className='dark:text-blue-400 text-blue-700 text-xl hover:underline'
-          >
-            walidhassan@tanim.com
-          </Link>
-        </div>
-        <div className='text-center shadow-xl dark:shadow-none bg-slate-100 dark:bg-slate-800 py-6'>
-          <FontAwesomeIcon
-            icon={faPhone}
-            className='text-3xl inline-block w-8'
-          />
-          <p className='text-2xl'>Phone</p>
-          <Link
-            href='tel:+01234567899'
-            className='dark:text-blue-400 text-blue-700 text-xl hover:underline'
-          >
-            +01234567899
-          </Link>
-        </div>
-        <div className='text-center shadow-xl dark:shadow-none bg-slate-100 dark:bg-slate-800 py-6'>
-          <FontAwesomeIcon
-            icon={faWhatsapp}
-            className='text-3xl inline-block w-8'
-          />
-          <p className='text-2xl'>WhatsApp</p>
-          <Link
-            href='whatsappAddress'
-            className='dark:text-blue-400 text-blue-700 text-xl hover:underline'
-          >
-            +01234567899
-          </Link>
-        </div>
-        <div className='text-center shadow-xl dark:shadow-none bg-slate-100 dark:bg-slate-800 py-6'>
-          <FontAwesomeIcon
-            icon={faLocationDot}
-            className='text-3xl inline-block w-8'
-          />
-          <p className='text-2xl'>Address</p>
-          <Link
-            href='location'
-            className='dark:text-blue-400 text-blue-700 text-xl hover:underline'
-          >
-            HSTU, Basherhat, Sadar, Dinajpur
-          </Link>
-        </div>
+        <ContactCard contactInfo={contact.email}>Email</ContactCard>
+        <ContactCard contactInfo={contact.phone}>Phone</ContactCard>
+        <ContactCard contactInfo={contact.whatsApp}>WhatsApp</ContactCard>
+        <ContactCard contactInfo={contact.address}>Address</ContactCard>
       </div>
     </section>
   );
