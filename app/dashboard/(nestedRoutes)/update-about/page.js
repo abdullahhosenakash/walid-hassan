3151cceb7a97e190622f1cf9e86bdfdf76ca4275
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-17 16:43:10 +0600

    bug fixed

--- app/dashboard/(nestedRoutes)/update-about/page.js
+++ app/dashboard/(nestedRoutes)/update-about/page.js
@@ -3,6 +3,7 @@ import UpdateAboutClient from '@/app/dashboard/_components/UpdateAboutClient';
 
 const UpdateAbout = async () => {
   const { aboutMe } = await getMiscellaneousData();
+  // console.log('')
   return (
     <section className='lg:w-1/2 mx-auto pb-12'>
       <h2 className='text-center lg:text-3xl text-xl mt-3'>Update About</h2>
