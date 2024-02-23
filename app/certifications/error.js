bea830618fb73fe6c74e7675e8081644bfcf7546
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-23 07:27:56 +0600

    certifications page layout, error and loading page added and also get api developed

---
+++ app/certifications/error.js
@@ -0,0 +1,18 @@
+'use client';
+
+const CertificationsError = ({ error, reset }) => {
+  return (
+    <section className='dark:bg-slate-900 lg:pt-4 pb-4 dark:text-white lg:px-0 px-3 dark:min-h-screen'>
+      <h2 className='text-center text-3xl dark:text-whit text-red-600 lg:mt-12'>
+        Something went wrong loading certifications!
+      </h2>
+      <button
+        className='block mx-auto mt-3 text-lg hover:text-pink-700 hover:underline'
+        onClick={() => reset()}
+      >
+        Please try again
+      </button>
+    </section>
+  );
+};
+export default CertificationsError;
---
+++ app/certifications/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Certifications - Walid Hassan',
+  description:
+    "This is Walid Hassan's certifications page of the portfolio website"
+};
+
+const CertificationsLayout = ({ children }) => {
+  return <section>{children}</section>;
+};
+export default CertificationsLayout;
