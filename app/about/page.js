ea18d9021812df8c484f558634dd55da59064c30
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-27 14:24:25 +0600

    about page loading skeleton added

--- app/about/page.js
+++ app/about/page.js
@@ -15,6 +15,7 @@ const About = async () => {
   return (
     <section className='dark:bg-slate-900 lg:pt-4 pb-4 dark:text-white lg:px-0 px-3 dark:min-h-screen'>
       <h2 className='text-center text-3xl dark:text-white'>About Me</h2>
+
       <div className='flex flex-col gap-4 text-lg mt-6 lg:w-[65%] mx-auto text-justify'>
         <p>Hi,</p>
         <p>
