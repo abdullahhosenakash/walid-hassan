77b10a34641c253c7b22c8866b089c9ef0234c88
abdullahhosenakash <abdullahhosenakash@gmail.com>
2024-02-27 16:16:37 +0600

    experience and skills page loading skeleton  added

--- app/experience/loading.js
+++ app/experience/loading.js
@@ -1,7 +1,30 @@
 const ExperienceLoading = () => {
   return (
     <section className='dark:bg-slate-900 lg:pt-4 pb-4 dark:text-white lg:px-0 px-3 dark:min-h-screen'>
-      <h2 className='text-3xl text-center'>Loading experience...</h2>
+      <h2 className='text-center text-3xl'>My Experience</h2>
+      <div className='flex flex-col gap-4 text-lg mt-6 lg:w-[65%] mx-auto text-justify animate-pulse'>
+        {/* fixed para */}
+        <div className='py-3 w-40 dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-1/2 dark:bg-slate-600 bg-slate-300 rounded-full' />
+
+        {/* first para */}
+        <div className='py-3 w-40 dark:bg-slate-600 bg-slate-300 rounded-full mt-4' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+
+        {/* second para */}
+        <div className='py-3 w-40 dark:bg-slate-600 bg-slate-300 rounded-full mt-4' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-3/4 dark:bg-slate-600 bg-slate-300 rounded-full' />
+
+        {/* third para */}
+        <div className='py-3 w-40 dark:bg-slate-600 bg-slate-300 rounded-full mt-4' />
+        <div className='py-2 w-full dark:bg-slate-600 bg-slate-300 rounded-full' />
+        <div className='py-2 w-1/4 dark:bg-slate-600 bg-slate-300 rounded-full' />
+      </div>
     </section>
   );
 };
--- app/skills/loading.js
+++ app/skills/loading.js
@@ -1,7 +1,41 @@
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const SkillsLoading = () => {
   return (
     <section className='dark:bg-slate-900 lg:pt-4 pb-4 dark:text-white lg:px-0 px-3 dark:min-h-screen'>
-      <h2 className='text-3xl text-center'>Loading skills...</h2>
+      <h2 className='text-center text-3xl dark:text-white'>My Skills</h2>
+      <div className='animate-pulse lg:w-[60%] mx-auto mt-12'>
+        <label className='relative inline-flex items-center'>
+          <input type='checkbox' className='sr-only peer' />
+          <div className='w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5' />
+          <span className='ms-3 text-sm font-medium text-gray-900 dark:text-gray-300'>
+            Show Progress
+          </span>
+        </label>
+
+        <div className='mt-8 flex flex-col gap-12'>
+          <div>
+            <div className='py-3 w-[20%] bg-slate-300 dark:bg-slate-600 rounded-full' />
+            <div className='py-2 w-1/2 bg-slate-300 dark:bg-slate-600 rounded-full mt-4' />
+          </div>
+          <div>
+            <div className='py-3 w-[20%] bg-slate-300 dark:bg-slate-600 rounded-full' />
+            <div className='py-2 w-3/4 bg-slate-300 dark:bg-slate-600 rounded-full mt-4' />
+          </div>
+          <div>
+            <div className='py-3 w-[20%] bg-slate-300 dark:bg-slate-600 rounded-full' />
+            <div className='py-2 w-1/4 bg-slate-300 dark:bg-slate-600 rounded-full mt-4' />
+          </div>
+        </div>
+        <p className='mt-6 flex gap-3 items-center text-slate-600 dark:text-slate-400'>
+          <span>
+            <FontAwesomeIcon icon={faClock} className='w-4 inline-block' /> Last
+            Updated:
+          </span>
+          <span className='inline-block py-2 w-1/4 bg-slate-300 dark:bg-slate-600 rounded-full' />
+        </p>
+      </div>
     </section>
   );
 };
